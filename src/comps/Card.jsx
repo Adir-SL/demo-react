559e563123c3822c2e58ba340aebbb2cc5ab26be
Adir <slutzkis@gmail.com>
2024-08-12 17:41:38 +0300

    updates

--- src/comps/Card.jsx
+++ src/comps/Card.jsx
@@ -3,7 +3,17 @@ import "./Card.css";
 
 const Card = (props) => {
     return (
-        <div></div>
+        <div class="card">
+            <input type="checkbox" />
+            <div class="content">
+                <div class="icon"></div>
+                <div class="inner-content">
+                    <b>{props.title}</b>
+                    <p>{props.section}</p>
+                </div>
+            </div>
+            <div class="tag">28 עדכונים</div>
+        </div>
     )
 };
 
--- src/comps/RightPanel.jsx
+++ src/comps/RightPanel.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
 import "./RightPanel.css";
+import Card from './Card'
 
 const RightPanel = (props) => {
     return (
-        <div className="right-panel"></div>
+        <div className="right-panel">
+            <Card
+                title="מפת שטח נשר"
+                section="9837463"
+            />
+            <Card
+                title="מפת שטח רכסים"
+                section="9736274"
+            />
+            <Card
+                title="מפת שטח יגור"
+                section="9853795"
+            />
+            <Card
+                title="מפת שטח חיפה"
+                section="9736274"
+            />
+            <Card
+                title="מפת שטח עין הוד"
+                section="9028467"
+            />
+            <Card
+                title="מפת שטח מגדים"
+                section="9036554"
+            />
+            <Card
+                title="מפת שטח הכרמל"
+                section="9278555"
+            />
+            <Card
+                title="מפת שטח תמרה"
+                section="9037555"
+            />
+        </div>
     )
 };
 
