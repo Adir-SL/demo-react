358d642276167d708f2365faa1118990a75199f0
Adir <slutzkis@gmail.com>
2024-08-13 19:52:11 +0300

    Update LocalSelectableCard.jsx

--- src/comps/LocalSelectableCard.jsx
+++ src/comps/LocalSelectableCard.jsx
@@ -2,13 +2,23 @@ import React from 'react';
 import "./LocalSelectableCard.css";
 
 const LocalSelectableCard = (props) => {
+    function clickFunc(event) {
+        if (event.target.parentNode.parentNode.parentNode.classList.contains('expanded')) {
+            event.target.parentNode.parentNode.parentNode.classList.remove('expanded');
+        } else {
+            event.target.parentNode.parentNode.parentNode.classList.add('expanded');
+        }
+    }
     return (
         <div class={(props.expanded ? 'card expanded' : 'card')}>
             <input type="checkbox" hidden={!props.checkbox} />
             <div class="content">
                 <div class="icon"></div>
                 <div class="inner-content">
-                    <b>{props.title}</b>
+                    <b onClick={(event) => {
+                        clickFunc(event)
+                    }}
+                    >{props.title}</b>
                     <p>{props.section}</p>
                 </div>
             </div>
