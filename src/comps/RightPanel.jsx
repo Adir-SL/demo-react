7543df110f3240c02714cbd50e78e848ae532893
Adir <slutzkis@gmail.com>
2024-08-13 12:09:57 +0300

    Update RightPanel.jsx

--- src/comps/RightPanel.jsx
+++ src/comps/RightPanel.jsx
@@ -1,39 +1,39 @@
 import React from 'react';
 import "./RightPanel.css";
-import Card from './LocalSelectableCard'
+import LocalSelectableCard from './LocalSelectableCard'
 
 const RightPanel = (props) => {
     return (
         <div className="right-panel">
-            <Card
+            <LocalSelectableCard
                 title="מפת שטח נשר"
                 section="9837463"
             />
-            <Card
+            <LocalSelectableCard
                 title="מפת שטח רכסים"
                 section="9736274"
             />
-            <Card
+            <LocalSelectableCard
                 title="מפת שטח יגור"
                 section="9853795"
             />
-            <Card
+            <LocalSelectableCard
                 title="מפת שטח חיפה"
                 section="9736274"
             />
-            <Card
+            <LocalSelectableCard
                 title="מפת שטח עין הוד"
                 section="9028467"
             />
-            <Card
+            <LocalSelectableCard
                 title="מפת שטח מגדים"
                 section="9036554"
             />
-            <Card
+            <LocalSelectableCard
                 title="מפת שטח הכרמל"
                 section="9278555"
             />
-            <Card
+            <LocalSelectableCard
                 title="מפת שטח תמרה"
                 section="9037555"
             />
