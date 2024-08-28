66b3da8af0b0abfb3af35143c8ed27acd6bdeb85
Adir <slutzkis@gmail.com>
2024-08-28 19:56:27 +0300

    Update Topbar.jsx

--- src/comps/Topbar.jsx
+++ src/comps/Topbar.jsx
@@ -3,7 +3,7 @@ import "./Topbar.css";
 
 const Topbar = (props) => {
     return (
-        <div className="top-bar"><h1>ברוכים הבאים לדמו דיי - קידו</h1></div>
+        <div className="top-bar"><h1>MapIt</h1></div>
     )
 };
 
