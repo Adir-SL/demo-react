3b08d7c5d0f82f47082462834328ecdec2a1023a
Adir <slutzkis@gmail.com>
2024-08-28 20:07:46 +0300

    Update Map.jsx

--- src/comps/Map.jsx
+++ src/comps/Map.jsx
@@ -3,7 +3,9 @@ import "./Map.css";
 
 const Map = (props) => {
     return (
-        <iframe className="map-frame" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26829.927158368308!2d35.123423045489524!3d32.7991288638577!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151db6a595965653%3A0x6c32c734af72d61f!2z16fXqNeZ16og15DXqteQ!5e0!3m2!1siw!2sil!4v1723101302077!5m2!1siw!2sil" width="600" height="450" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+        // <iframe className="map-frame" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26829.927158368308!2d35.123423045489524!3d32.7991288638577!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151db6a595965653%3A0x6c32c734af72d61f!2z16fXqNeZ16og15DXqteQ!5e0!3m2!1siw!2sil!4v1723101302077!5m2!1siw!2sil" width="600" height="450" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+
+        <iframe className="map-frame" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3369.549578242515!2d34.89961452348927!3d32.37762657383308!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1siw!2sil!4v1724864581942!5m2!1siw!2sil" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     )
 };
 
